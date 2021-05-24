f269598c994d20b1dc209220b59b31fd5dc9e2ed
Oleg <brezitskyyoleg@gmail.com>
2021-05-24 16:54:27 +0200

    Typos

--- queries.sql
+++ queries.sql
@@ -2,11 +2,11 @@
 --  1. simple query with WHERE conditions
 -- ***************************************************
 
--- Select bateriums's ids of aerobic bacterias
+-- Select bateriums's ids of aerobic bacterias.
 SELECT idBacterium FROM Bacterium WHERE OxygenDemand='Aerobic';
 
 -- Select the diseases and their symptoms that causes the bacterium
--- id="123123123"
+-- id="123123123".
 SELECT NameDisease, Symptoms
 FROM Disease
 WHERE Bacterium_idBacterium='123123123'
@@ -17,7 +17,7 @@ WHERE Bacterium_idBacterium='123123123'
 --  2. query using JOIN and WHERE conditions
 -- ***************************************************
 
--- Select bacterium's ids of bacteria whose genome cat. is "plasmid"
+-- Select bacterium's ids of bacteria whose genome cat. is "plasmid".
 SELECT Bacterium.idBacterium
 FROM Bacterium
 FULL OUTER JOIN Genome ON Bacterium.idBacterium=Genome.Bacterium_idBacterium
@@ -29,13 +29,13 @@ WHERE Genome.Category='PLASMID';
 --  3. query including subqueries and WHERE conditions
 -- ***************************************************
 
--- Get bacterium's id="123123123" genome length
+-- Get bacterium's id="123123123" genome length.
 SELECT SUM(SecLen)
 FROM (SELECT LENGTH(Secuence) AS SecLen
       FROM Gen
       WHERE Genome_idGenome='123123123')
 
--- Select all diseases that cause the coccus bacteria
+-- Select all diseases that cause the coccus bacteria.
 SELECT NameDisease
 FROM Disease
 WHERE Bacterium_idBacterium IN (
